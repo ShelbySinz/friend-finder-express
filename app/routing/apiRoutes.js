6720351cf98e156d94e753023a0d72f0d9b925a1
Shelby2 <shelbsjb@hotmail.com>
2019-02-24 18:30:39 -0700

    fixed the modal

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,5 @@
 
-var friendData = require("../data/friends");
+var friendData = require("../data/friends.js");
 var path = require("path");
 
 
@@ -20,31 +20,36 @@ module.exports = function(app) {
 
   app.post('/api/friends', function(req,res){
     //grabs the new friend's scores to compare with friends in friend array
+   //console.log(req.body);
     var userInput = req.body;
     var newFriendScores = req.body.surveyAnswers;
     var matchName = '';
-    var matchImage = 0;
+    var matchImage = '';
     var totalDifference = 1000;
-
+    var friends = friendData;
     //runs through all current friends in list
-    for(var i=0; i < friendData.length; i++){
+    for(var i=0; i < friends.length; i++){
       var scoresDiff = 0;
       //run through scores to compare friends
+     
       for(var j=0; j < newFriendScores.length; j++){
-        scoresDiff += Math.abs(friendData[i].surveyAnswers-newFriendScores[j]);
+        scoresDiff += Math.abs(friends[i].surveyAnswers[j]- newFriendScores[j]);
       }
-
+      // console.log(friends[i].surveyAnswers)
+      // console.log(newFriendScores);
+       console.log(scoresDiff);
     if(scoresDiff < totalDifference){
-        totalDifference = scoresDiff;
-        matchName = friendData[i].name;
-        matchImage = friendsData[i].photo;
+        
+        matchName = friends[i].Name;
+        matchImage = friends[i].photo;
     }
   }
-
+ 
 // Add new user
 friendData.push(userInput);
 
 // Send appropriate response
+console.log(matchName);
 res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
 });
 };
